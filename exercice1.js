7649087a7a719facc90bcd905d0ef766f63e4d44
ChloeArd <chloe.ardoise@gmail.com>
2020-12-14 20:41:37 +0100

    Exercice 84

--- exercice1.js
+++ exercice1.js
@@ -13,8 +13,33 @@ let articles = [
 // Votre code à partir d'ici :
 let listeCourse = document.getElementById("listeCourse")
 let liste = document.createElement('li');
-liste.innerHTML = articles;
+articles.push("Chocolat", "Coca-Cola")
+liste.innerHTML = articles ;
 listeCourse.appendChild(liste);
+
+let button = document.getElementById("button1");
+//Ajouter un article à la liste de course
+let ajout = function () {
+    let input1 = document.getElementById("input1").value;
+    articles.push(input1);
+    liste.innerHTML = articles;
+    listeCourse.appendChild(liste);
+}
+
+button.addEventListener("click", ajout);
+
+
+// supprimer le dernier article de la liste de course.
+let supprimer = document.getElementById("button2");
+supprimer.addEventListener("click", function () {
+    articles.pop();
+    liste.innerHTML = articles;
+});
+
+
+
+
+
 
 
 
